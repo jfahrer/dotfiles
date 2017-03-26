9515dcacb3c0c3519cfd3d247b03e1fd39c40097
Julian Fahrer <julian@fahrer.net>
2017-03-26 12:41:27 -0700

    Add a railsrc and template

---
+++ rails_template.rb
@@ -0,0 +1,30 @@
+gem 'haml-rails'
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
+  gem "capybara"
+  gem "factory_girl_rails"
+  gem "rspec-rails"
+  gem "byebug"
+  gem "shoulda-matchers"
+end
+
+gem_group :development, :test do
+  gem 'irbtools', require: 'irbtools/binding'
+end
+
+run("bundle install")
+generate("rspec:install")
+
+run("bundle exec spring binstub rspec")
+run("bundle exec spring binstub")
+
+rake("db:create")
+
+run "rm README.rdoc"
+run "echo '# #{@app_name.titleize}' >> README.md"
+
+git :init
+git add: "."
+git commit: "-a -m initial"
