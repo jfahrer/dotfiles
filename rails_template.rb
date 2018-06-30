5595aea41c81959c08f794ddd17c3e9a118b6c25
Julian Fahrer <julian@fahrer.net>
2018-06-29 17:06:42 -0700

    Modernize the rails template - Create a Docker and Compose file - Use FactoryBot instead of FactoryGirl

--- rails_template.rb
+++ rails_template.rb
@@ -1,40 +1,71 @@
 gem 'haml-rails'
 
 gem_group :development, :test do
-  gem 'rspec-rails'
-  gem 'spring-commands-rspec'
-  gem "capybara"
-  gem "factory_girl_rails"
-  gem "rspec-rails"
   gem "byebug"
+  gem "capybara"
+  gem "factory_bot_rails"
+  gem "rspec-rails"
   gem "shoulda-matchers"
+  gem 'spring-commands-rspec'
 end
 
 gem_group :development, :test do
-  gem 'irbtools', require: 'irbtools/binding'
   gem 'awesome_print'
+  gem 'irbtools', require: 'irbtools/binding'
 end
 
 run("bundle install")
 generate("rspec:install")
 
-run("bundle exec spring binstub rspec")
-run("bundle exec spring binstub")
-
-rake("db:create")
+run("bundle exec spring binstub rails rake rspec")
 
 run "rm README.rdoc"
 run "echo '# #{@app_name.titleize}' >> README.md"
 
-git :init
-git add: "."
-git commit: "-a -m initial"
-
-file 'Dockerfile', <<-CODE
-CODE
+run "rake haml:erb2haml"
 
 file 'docker-compose.yml', <<-CODE
+version: "3"
+
+services:
+  pg:
+    image: postgres:9.6-alpine
+    ports:
+      - 5432:5432
+    volumes:
+      - pg-data:/var/lib/postgresql/data
+
+volumes:
+  pg-data:
 CODE
 
-file 'docker-compose.dev.yml', <<-CODE
+file 'config/database.yml', <<-CODE
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  # For details on connection pooling, see Rails configuration guide
+  # http://guides.rubyonrails.org/configuring.html#database-pooling
+  pool: <%= ENV.fetch('RAILS_MAX_THREADS') { 5 } %>
+
+  username: <%= ENV.fetch('PG_USERNAME', 'postgres') %>
+  password: <%= ENV.fetch('PG_USERNAME', '') %>
+  host: <%= ENV.fetch('PG_HOST', 'localhost') %>
+  port: <%= ENV.fetch('PG_PORT', 5432) %>
+
+development:
+  <<: *default
+  database: #{@app_name}_development
+
+test:
+  <<: *default
+  database: #{@app_name}_test
+
+production:
+  <<: *default
+  database: #{@app_name}_production
 CODE
+
+git :init
+git add: '.'
+git commit: "-a -m 'Initial commit'"
+
