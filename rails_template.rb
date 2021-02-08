845206433167a33ebb081badb09d7c7f97b3797d
Julian Fahrer <julian@fahrer.net>
2021-02-08 13:02:12 -1000

    Run standard over the rails template

--- rails_template.rb
+++ rails_template.rb
@@ -1,25 +1,22 @@
-gem 'haml-rails'
+gem "haml-rails"
 
 gem_group :development, :test do
   gem "byebug"
   gem "capybara"
   gem "factory_bot_rails"
   gem "rspec-rails"
-  gem 'spring-commands-rspec'
+  gem "spring-commands-rspec"
 end
 
 gem_group :development, :test do
-  gem 'awesome_print'
-  gem 'irbtools', require: 'irbtools/binding'
+  gem "awesome_print"
+  gem "irbtools", require: "irbtools/binding"
 end
 
 after_bundle do
   generate("rspec:install")
 
-  run "rm README.rdoc"
-  run "echo '# #{@app_name.titleize}' >> README.md"
-
   git :init
-  git add: '.'
+  git add: "."
   git commit: "-a -m 'Initial commit'"
 end
