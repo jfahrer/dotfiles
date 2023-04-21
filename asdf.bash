b111c5bf9c557bb4f67ba647c8a7db8560395f3d
Julian Fahrer <julian@fahrer.net>
2023-04-21 16:15:36 -0700

    Fix asdf initialization

--- asdf.bash
+++ asdf.bash
@@ -1,6 +1,6 @@
 # Setup asdf
 
-if [ ! -x $(which brew) ] && [ ! -z $(which asdf) ]; then
+if [ -x $(which brew) ] && [ -x $(which asdf) ]; then
   . $(brew --prefix asdf)/libexec/asdf.sh
   . $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
 fi
