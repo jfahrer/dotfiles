9bdf0d3d99ebebf9de9ecf0a7e6f9ae028faf19a
Julian Fahrer <julian@fahrer.net>
2025-09-15 09:32:17 -0700

    Fix asdf bash completion path

--- asdf.bash
+++ asdf.bash
@@ -2,5 +2,5 @@
 
 if [ -x $(which brew) ] && [ -x $(which asdf) ]; then
   . $(brew --prefix asdf)/libexec/asdf.sh
-  . $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+  . $(brew --prefix asdf)/etc/bash_completion.d/asdf
 fi
