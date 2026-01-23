08619df8bd3e8e34d0e559dad7851778512f017d
Julian Fahrer <julian@fahrer.net>
2026-01-22 17:18:50 -0800

    Fix keymap description

--- config/nvim/lua/plugins/opencode.lua
+++ config/nvim/lua/plugins/opencode.lua
@@ -10,7 +10,7 @@ return {
     { "<leader>aa", function() require("opencode").ask("@cursor: ") end, desc = "Ask opencode about this", mode = "n" },
     { "<leader>aa", function() require("opencode").ask("@selection: ") end, desc = "Ask opencode about selection", mode = "v" },
     { "<leader>an", function() require("opencode").command("session_new") end, desc = "New opencode session" },
-    { "<leader>ay", function() require("user.opencode").copy_last_message_to_buffer() end, desc = "Copy opencode message to buffer" },
+    { "<leader>ay", function() require("user.opencode").copy_last_message_to_buffer() end, desc = "Paste opencode response to tmp buffer" },
     { "<leader>aY", function() require("opencode").command("messages_copy") end, desc = "Copy last opencode response" },
     { "<S-C-u>", function() require("opencode").command("messages_half_page_up") end, desc = "Messages half page up" },
     { "<S-C-d>", function() require("opencode").command("messages_half_page_down") end, desc = "Messages half page down" },
