74d5b5c1ab28955fbb4e3461a809612c445cbbac
Julian Fahrer <julian@fahrer.net>
2026-03-13 23:03:32 -0700

    Invoke nvim instead of vim

--- fzf.bash
+++ fzf.bash
@@ -16,4 +16,4 @@ export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
 export FZF_DEFAULT_OPTS='--bind ctrl-j:down,ctrl-k:up,ctrl-a:select-all,ctrl-d:deselect-all'
 bind "$(bind -s | grep __fzf_cd__ | sed 's/\\ec/\\C-g/')"
 bind "$(bind -s | grep __fzf_select | sed 's/\\C-t/\\C-n/')"
-bind -x '"\C-p": $(fzf | xargs -r echo vim);'
+bind -x '"\C-p": $(fzf | xargs -r echo nvim);'
