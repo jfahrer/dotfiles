da8e6520e5d004e6e6e8357b66362ae0a17d9708
Julian Fahrer <julian@fahrer.net>
2026-03-18 18:30:04 -0700

    Leave insert mode when navigating to another split

--- config/nvim/lua/plugins/smart-splits.lua
+++ config/nvim/lua/plugins/smart-splits.lua
@@ -2,14 +2,46 @@ return {
   "mrjones2014/smart-splits.nvim",
   lazy = false,
   keys = {
-    { "<A-h>", function() require("smart-splits").resize_left() end },
-    { "<A-j>", function() require("smart-splits").resize_down() end },
-    { "<A-k>", function() require("smart-splits").resize_up() end },
-    { "<A-l>", function() require("smart-splits").resize_right() end },
+    { "<A-h>", function() require("smart-splits").resize_left() end, mode = { "n", "i" } },
+    { "<A-j>", function() require("smart-splits").resize_down() end, mode = { "n", "i" } },
+    { "<A-k>", function() require("smart-splits").resize_up() end, mode = { "n", "i" } },
+    { "<A-l>", function() require("smart-splits").resize_right() end, mode = { "n", "i", "v" } },
     { "<C-h>", function() require("smart-splits").move_cursor_left() end },
     { "<C-j>", function() require("smart-splits").move_cursor_down() end },
     { "<C-k>", function() require("smart-splits").move_cursor_up() end },
     { "<C-l>", function() require("smart-splits").move_cursor_right() end },
+    {
+      "<C-h>",
+      function()
+        vim.cmd("stopinsert")
+        require("smart-splits").move_cursor_left()
+      end,
+      mode = "i",
+    },
+    {
+      "<C-j>",
+      function()
+        vim.cmd("stopinsert")
+        require("smart-splits").move_cursor_down()
+      end,
+      mode = "i",
+    },
+    {
+      "<C-k>",
+      function()
+        vim.cmd("stopinsert")
+        require("smart-splits").move_cursor_up()
+      end,
+      mode = "i",
+    },
+    {
+      "<C-l>",
+      function()
+        vim.cmd("stopinsert")
+        require("smart-splits").move_cursor_right()
+      end,
+      mode = "i",
+    },
   },
   config = function()
     require("smart-splits").setup({
@@ -19,4 +51,3 @@ return {
     })
   end,
 }
-
